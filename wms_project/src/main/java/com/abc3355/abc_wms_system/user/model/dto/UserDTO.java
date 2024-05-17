0a5860dd3e2f81733e5c1d55d9e65a25ae017f8d
yujin4sth00 <yujin4sth00@gmail.com>
2024-05-17 16:52:35 +0900

    로그인 패키지 1차

---
+++ wms_project/src/main/java/com/abc3355/abc_wms_system/user/model/dto/UserDTO.java
@@ -0,0 +1,15 @@
+package com.abc3355.abc_wms_system.user.model.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class UserDTO {
+    private int userNo;
+    private String userId;
+    private String userPassword;
+    private String userType;
+}
---
+++ wms_project/src/main/java/com/abc3355/abc_wms_system/user/view/LoginView.java
@@ -0,0 +1,14 @@
+package com.abc3355.abc_wms_system.user.view;
+
+import com.abc3355.abc_wms_system.user.controller.LoginController;
+
+import java.util.Scanner;
+
+public class LoginView {
+    private LoginController loginController = new LoginController();
+
+    public void displayLoginMenu(){
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
