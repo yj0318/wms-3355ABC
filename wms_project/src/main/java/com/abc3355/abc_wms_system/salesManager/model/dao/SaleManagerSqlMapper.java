f7fa8b679629ec1e082d34ab961a1c4c348c0c25
DISNOTACAT <yeonjh915@gmail.com>
2024-05-18 15:18:46 +0900

    #7 가맹점별 조회 기능 추가

--- wms_project/src/main/java/com/abc3355/abc_wms_system/salesManager/model/dao/SaleManagerSqlMapper.java
+++ wms_project/src/main/java/com/abc3355/abc_wms_system/salesManager/model/dao/SaleManagerSqlMapper.java
@@ -1,4 +1,14 @@
 package com.abc3355.abc_wms_system.salesManager.model.dao;
 
+import com.abc3355.abc_wms_system.salesManager.model.dto.BranchDTO;
+import com.abc3355.abc_wms_system.salesManager.model.dto.OrderProductDTO;
+import com.abc3355.abc_wms_system.salesManager.model.dto.SelectBranchAndDateDTO;
+
+import java.util.List;
+
 public interface SaleManagerSqlMapper {
+
+    List<OrderProductDTO> selectByBranchAndDate(SelectBranchAndDateDTO selectDTO);
+
+    List<BranchDTO> getAllBranch();
 }
