424eb0f02b09fd4bec6c2c86057c6301f1d013fb
Gyunorld <mingue640@gmail.com>
2024-05-19 17:11:33 +0900

    상품 상세 조회

--- wms_project/src/main/java/com/abc3355/abc_wms_system/SearchProduct/model/dao/SizeMapper.java
+++ wms_project/src/main/java/com/abc3355/abc_wms_system/SearchProduct/model/dao/SizeMapper.java
@@ -1,12 +1,9 @@
 package com.abc3355.abc_wms_system.searchProduct.model.dao;
 
 import com.abc3355.abc_wms_system.searchProduct.model.dto.SearchProductDTO;
-import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
-public interface ColorMapper {
-
-
-    List<SearchProductDTO> searchProductByColor(String colors);
+public interface SizeMapper {
+    List<SearchProductDTO> searchBySize(int size);
 }
