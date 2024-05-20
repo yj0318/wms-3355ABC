02eef3c691ea8e1c7121e1613598c241dcdf9178
yujin4sth00 <yujin4sth00@gmail.com>
2024-05-21 08:52:58 +0900

    pull받기

--- wms_project/src/main/java/com/abc3355/abc_wms_system/user/view/LoginView.java
+++ wms_project/src/main/java/com/abc3355/abc_wms_system/user/view/LoginView.java
@@ -19,7 +19,13 @@ public class LoginView {
     public static UserDTO user;
     private static Map<String, String> loginMatch;
     private UserAndWarehouseDTO newUserAndWarehouse;
-    private UserAndWarehouseDTO deleteUserAndWarehouse;
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_BLUE = "\u001B[34m";
+    private static final String ANSI_PURPLE = "\u001B[35m";
+    private static final String ANSI_CYAN = "\u001B[36m";
 
     /*Controller선언 부분*/
     private LoginController loginController = new LoginController();
@@ -31,9 +37,10 @@ public class LoginView {
     public void loginFirstMenu() {
         Scanner sc = new Scanner(System.in);
         do {
+            System.out.println("==== 로그인 ====");
             System.out.println("1. 본사");
             System.out.println("2. 가맹점");
-            System.out.println("0. 종료하기");
+            System.out.println(ANSI_BLUE + "0. 종료하기" + ANSI_RESET);
             System.out.print("해당하는 계정을 선택하세요: ");
             int no = sc.nextInt();
             sc.nextLine();
@@ -69,6 +76,7 @@ public class LoginView {
     /* 본사 로그인 화면 */
     private Map<String, String> headLoginFirstMenu() {
         Scanner sc = new Scanner(System.in);
+        System.out.println("==================");
         System.out.println("본사 로그인");
         System.out.print("ID: ");
         String userId = sc.nextLine();
@@ -83,6 +91,7 @@ public class LoginView {
     /* 가맹점 로그인 화면 */
     private Map<String, String> branchLoginFirstMenu() {
         Scanner sc = new Scanner(System.in);
+        System.out.println("==================");
         System.out.println("가맹점 로그인");
         System.out.print("ID : ");
         String userId = sc.nextLine();
@@ -97,14 +106,14 @@ public class LoginView {
     /* 본사 메뉴 */
     private void headMainMenu() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("< 본사 > 메뉴");
+        System.out.println("==== " + ANSI_PURPLE + "본사" + ANSI_RESET +" 메뉴 ====");
         while (true){
-            System.out.println("1. 주문관리");
-            System.out.println("2. 재고조회");
-            System.out.println("3. 상품관리");
-            System.out.println("4. 매출관리");
-            System.out.println("5. 가맹점관리");
-            System.out.println("0. 로그아웃");
+            System.out.println(ANSI_BLUE +"1. 주문관리" + ANSI_RESET);
+            System.out.println(ANSI_BLUE +"2. 재고조회" + ANSI_RESET);
+            System.out.println(ANSI_BLUE +"3. 상품관리" + ANSI_RESET);
+            System.out.println(ANSI_BLUE +"4. 매출관리" + ANSI_RESET);
+            System.out.println(ANSI_BLUE +"5. 가맹점관리" + ANSI_RESET);
+            System.out.println("0. 로그아웃" );
             System.out.print("메뉴 선택 : ");
             int menu = sc.nextInt();
             sc.nextLine();
@@ -131,12 +140,12 @@ public class LoginView {
     /* 가맹점 메뉴 */
     private void branchMainMenu(String whId) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("< " + whId + " 지점> 메뉴");
+        System.out.println( "==== " + ANSI_RED +"< "+ whId + ">"+ANSI_RESET +" 지점 메뉴 ====");
         while (true){
-            System.out.println("1. 상품조회");
-            System.out.println("2. 주문서 작성");
-            System.out.println("3. 재고 조회");
-            System.out.println("0. 로그아웃");
+            System.out.println(ANSI_BLUE +"1. 상품조회" + ANSI_RESET);
+            System.out.println(ANSI_BLUE +"2. 주문서 작성" + ANSI_RESET);
+            System.out.println(ANSI_BLUE +"3. 재고 조회" + ANSI_RESET);
+            System.out.println( "0. 로그아웃");
             System.out.print("메뉴 선택 : ");
             int menu = sc.nextInt();
             sc.nextLine();
@@ -156,16 +165,16 @@ public class LoginView {
     private UserAndWarehouseDTO inputBranch() {
         Scanner sc = new Scanner(System.in);
         UserAndWarehouseDTO userAndWarehouse = new UserAndWarehouseDTO();
-        System.out.println("현재 등록된 가맹점");
+        System.out.println("===== 현재 등록된 가맹점 ====");
         addUserController.showAllBranch();
-        System.out.println("추가할 가맹점 정보를 입력하세요.");
+        System.out.println("==== 추가할 가맹점 정보 입력 ====");
         System.out.print("가맹점 ID: ");
         userAndWarehouse.setUserId(sc.nextLine());
         System.out.print("가맹점 Password: ");
         userAndWarehouse.setUserPassword(sc.nextLine());
-        System.out.print("지점 종류: ");
+        System.out.print("지점 종류(branch/head): ");
         userAndWarehouse.setUserType(sc.nextLine());
-        System.out.println("창고 정보를 입력하세요.");
+        System.out.println("==== 창고 정보 입력 ====");
         System.out.print("창고 이름: ");
         userAndWarehouse.setWhName(sc.nextLine());
         System.out.print("창고 주소: ");
@@ -178,9 +187,10 @@ public class LoginView {
     /* 가맹점 관리 메소드 */
     private void branchControl() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("1. 가맹점 조회");
-        System.out.println("2. 가맹점 추가");
-        System.out.println("3. 가맹점 삭제");
+        System.out.println("==================");
+        System.out.println(ANSI_BLUE +"1. 가맹점 조회"+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"2. 가맹점 추가"+ANSI_RESET);
+        System.out.println(ANSI_BLUE +"3. 가맹점 삭제"+ANSI_RESET);
         System.out.println("0. 이전 메뉴");
         System.out.print("관리할 메뉴 선택: ");
         int num = sc.nextInt();
@@ -200,16 +210,17 @@ public class LoginView {
         }
     }
 
+    /* 가맹점 삭제 기능 */
     private Map<String, String> deleteBranch() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("현재 등록된 가맹점");
+        System.out.println("=== 현재 등록된 가맹점 ====");
         addUserController.showAllBranch();
+        System.out.println("======================");
         System.out.print("삭제할 가맹점 번호: ");
         String no = sc.nextLine();
 
         Map<String, String> delete = new HashMap<>();
         delete.put("no", no);
-
         return delete;
     }
 }
