02614362048b40f173248704cf0289f8473bf704
yj0318 <dbswjd0318@gmail.com>
2024-05-20 18:08:29 +0900

    commit "주문서작성"

---
+++ wms_project/src/main/java/com/abc3355/abc_wms_system/orderForm/testApp.java
@@ -0,0 +1,10 @@
+package com.abc3355.abc_wms_system.orderForm;
+
+import com.abc3355.abc_wms_system.orderForm.view.OrderFormView;
+
+public class testApp {
+    public static void main(String[] args) {
+        OrderFormView orderFormView = new OrderFormView();
+        orderFormView.orderFormMain();
+    }
+}
