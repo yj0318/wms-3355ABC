4c298379737dff85f94abd196d1686344d672088
Gyunorld <mingue640@gmail.com>
2024-05-20 09:37:22 +0900

    commit

--- wms_project/src/main/java/com/abc3355/abc_wms_system/searchProduct/model/dto/InventoryDTO.java
+++ wms_project/src/main/java/com/abc3355/abc_wms_system/searchProduct/model/dto/InventoryDTO.java
@@ -2,6 +2,8 @@ package com.abc3355.abc_wms_system.searchProduct.model.dto;
 
 import lombok.*;
 
+import java.util.List;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -12,4 +14,17 @@ public class InventoryDTO {
     private int inventoryAmount;
     private int whId;
     private int productNo;
+    private SearchProductDTO productDTO;
+    private SearchColorDTO colorDTO;
+    private SearchSizeDTO sizeDTO;
+    private SearchCategoryDTO categoryDTO;
+    private String productName;
+    private int productPrice;
+    private String isExpire;
+    private int categoryNo;
+    private String categoryName;
+    private int colorNo;
+    private String colorName;
+    private int sizeNo;
+    private int sizeMm;
 }
