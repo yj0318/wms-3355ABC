13eeeebb19881ab0679a5ddff91d81fd053590de
yujin4sth00 <yujin4sth00@gmail.com>
2024-05-21 11:55:02 +0900

    메뉴 ui 수정

--- wms_project/src/main/java/com/abc3355/abc_wms_system/user/view/LoginView.java
+++ wms_project/src/main/java/com/abc3355/abc_wms_system/user/view/LoginView.java
@@ -2,6 +2,7 @@ package com.abc3355.abc_wms_system.user.view;
 
 import com.abc3355.abc_wms_system.Product.view.ProductView;
 import com.abc3355.abc_wms_system.orderForm.view.OrderFormView;
+import com.abc3355.abc_wms_system.orderProcess.view.BranchesMenuView;
 import com.abc3355.abc_wms_system.orderProcess.view.ManagerMenuView;
 import com.abc3355.abc_wms_system.salesManager.View.SalesManagerView;
 import com.abc3355.abc_wms_system.searchProduct.view.InventorySearchView;
@@ -33,15 +34,20 @@ public class LoginView {
     private AddUserController addUserController = new AddUserController();
     private ManagerMenuView managerMenuView = new ManagerMenuView();
     private ProductView productView = new ProductView();
+    private BranchesMenuView branchesMenuView = new BranchesMenuView();
+    private OrderFormView orderFormView = new OrderFormView();
+    private InventorySearchView inventorySearchView = new InventorySearchView();
+    private SearchMenuView searchMenuView = new SearchMenuView();
+
 
 
     public void loginFirstMenu() {
         Scanner sc = new Scanner(System.in);
         do {
-            System.out.println("==== 로그인 ====");
+            System.out.println("==== " + ANSI_BLUE + "로그인" +ANSI_RESET +" ======================================================================================");
             System.out.println("1. 본사");
             System.out.println("2. 가맹점");
-            System.out.println(ANSI_BLUE + "0. 종료하기" + ANSI_RESET);
+            System.out.println("9. 종료하기");
             System.out.print("해당하는 계정을 선택하세요: ");
             int no = sc.nextInt();
             sc.nextLine();
@@ -50,23 +56,23 @@ public class LoginView {
                     loginMatch = headLoginFirstMenu();
                     user = loginController.headLoginSecondMenu(loginMatch);
                     if (user != null && user.getUserType().equals("head")) {
-                        System.out.println("본사 로그인 성공");
+                        System.out.println("본사 로그인에 성공하셨습니다.");
                         headMainMenu();
                     } else {
-                        System.out.println("본사 로그인 실패");
+                        System.out.println("본사 로그인에 실패하셨습니다.");
                     }
                     break;
                 case 2:
                     loginMatch = branchLoginFirstMenu();
                     user = loginController.branchLoginSecondMenu(loginMatch);
                     if (user != null && user.getUserType().equals("branch")) {
-                        System.out.println("가맹점 로그인 성공");
+                        System.out.println("가맹점 로그인에 성공하셨습니다.");
                         branchMainMenu(user.getUserId());
                     } else {
-                        System.out.println("가맹점 로그인 실패");
+                        System.out.println("가맹점 로그인에 실패하셨습니다.");
                     }
                     break;
-                case 0:
+                case 9:
                     return;
                 default:
                     System.out.println("잘못 입력하셨습니다.");
@@ -77,8 +83,7 @@ public class LoginView {
     /* 본사 로그인 화면 */
     private Map<String, String> headLoginFirstMenu() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("==================");
-        System.out.println("본사 로그인");
+        System.out.println("==== " + ANSI_BLUE+"본사 로그인"+ANSI_RESET + " ===================================================================================");
         System.out.print("ID: ");
         String userId = sc.nextLine();
         System.out.print("PW: ");
@@ -92,8 +97,7 @@ public class LoginView {
     /* 가맹점 로그인 화면 */
     private Map<String, String> branchLoginFirstMenu() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("==================");
-        System.out.println("가맹점 로그인");
+        System.out.println("==== " + ANSI_BLUE+"가맹점 로그인"+ANSI_RESET + " ===============================================================================");
         System.out.print("ID : ");
         String userId = sc.nextLine();
         System.out.print("PW: ");
@@ -107,29 +111,30 @@ public class LoginView {
     /* 본사 메뉴 */
     private void headMainMenu() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("==== " + ANSI_PURPLE + "본사" + ANSI_RESET +" 메뉴 ====");
+        System.out.println("==== " + ANSI_PURPLE + "< 본사 >" + ANSI_RESET +" 메뉴 ================================================================================");
         while (true){
-            System.out.println(ANSI_BLUE +"1. 주문관리" + ANSI_RESET);
-            System.out.println(ANSI_BLUE +"2. 재고조회" + ANSI_RESET);
-            System.out.println(ANSI_BLUE +"3. 상품관리" + ANSI_RESET);
-            System.out.println(ANSI_BLUE +"4. 매출관리" + ANSI_RESET);
-            System.out.println(ANSI_BLUE +"5. 가맹점관리" + ANSI_RESET);
-            System.out.println("0. 로그아웃" );
-            System.out.print("메뉴 선택 : ");
+            System.out.println("1. 주문관리" );
+            System.out.println("2. 재고조회" );
+            System.out.println("3. 상품관리" );
+            System.out.println("4. 매출관리" );
+            System.out.println("5. 가맹점관리");
+            System.out.println("9. 로그아웃"  );
+            System.out.println("=================================================================================================");
+            System.out.print("메뉴 선택: ");
             int menu = sc.nextInt();
             sc.nextLine();
             switch (menu){
                 case 1: managerMenuView.mainMenu(); break;
-                case 2: new InventorySearchView().searchMenu(); break;
+                case 2: inventorySearchView.searchMenu(); break;
                 case 3:
-                    try { new ProductView().mainMenu();} catch (IOException e) { throw new RuntimeException(e);}
+                    try { productView.mainMenu();} catch (IOException e) { throw new RuntimeException(e);}
                     break;
                 case 4: SalesManagerView.SalesManagerMain(); break;
                 case 5: branchControl(); break;
-                case 0:
+                case 9:
                     return;
                 default:
-                    System.out.println("잘못 입력하셨습니다.");
+                    System.out.println("잘못된 값입니다. 다시 입력해주세요.");
             }
         }
     }
@@ -137,23 +142,25 @@ public class LoginView {
     /* 가맹점 메뉴 */
     private void branchMainMenu(String whId) {
         Scanner sc = new Scanner(System.in);
-        System.out.println( "==== " + ANSI_RED +"< "+ whId + ">"+ANSI_RESET +" 지점 메뉴 ====");
+        System.out.println( "==== "+ ANSI_PURPLE +"< "+ whId + ">" + ANSI_RESET +" 지점 메뉴 ========================================================================");
         while (true){
-            System.out.println(ANSI_BLUE +"1. 상품조회" + ANSI_RESET);
-            System.out.println(ANSI_BLUE +"2. 주문서 작성" + ANSI_RESET);
-            System.out.println(ANSI_BLUE +"3. 재고 조회" + ANSI_RESET);
-            System.out.println( "0. 로그아웃");
-            System.out.print("메뉴 선택 : ");
+            System.out.println("1. 주문관리" );
+            System.out.println("2. 재고 조회" );
+            System.out.println("3. 상품조회" );
+            System.out.println("4. 주문서 작성");
+            System.out.println( "9. 로그아웃" );
+            System.out.println("=================================================================================================");
+            System.out.print("메뉴를 선택하세요 : ");
             int menu = sc.nextInt();
             sc.nextLine();
             switch (menu){
-                case 1: new SearchMenuView().searchMenu(); break;
-                case 2: new OrderFormView().orderFormMain();break;
-                case 3: new InventorySearchView().searchMenu(); break;
-                case 4:
-                    return;
+                case 1 : branchesMenuView.mainMenu(); break;
+                case 2: inventorySearchView.searchMenu(); break;
+                case 3: searchMenuView.searchMenu(); break;
+                case 4: orderFormView.orderFormMain();break;
+                case 9: return;
                 default:
-                    System.out.println("잘못 입력하셨습니다.");
+                    System.out.println("잘못된 값입니다. 다시 입력해주세요.");
             }
         }
     }
@@ -162,16 +169,17 @@ public class LoginView {
     private UserAndWarehouseDTO inputBranch() {
         Scanner sc = new Scanner(System.in);
         UserAndWarehouseDTO userAndWarehouse = new UserAndWarehouseDTO();
-        System.out.println("===== 현재 등록된 가맹점 ====");
+        System.out.println("=================================================================================================");
+        System.out.println("==== 현재 등록된 가맹점 ============================================================================");
         addUserController.showAllBranch();
-        System.out.println("==== 추가할 가맹점 정보 입력 ====");
+        System.out.println("==== 추가할 가맹점 정보 입력 ========================================================================");
         System.out.print("가맹점 ID: ");
         userAndWarehouse.setUserId(sc.nextLine());
         System.out.print("가맹점 Password: ");
         userAndWarehouse.setUserPassword(sc.nextLine());
         System.out.print("지점 종류(branch/head): ");
         userAndWarehouse.setUserType(sc.nextLine());
-        System.out.println("==== 창고 정보 입력 ====");
+        System.out.println("==== 창고 정보 입력 ===============================================================================");
         System.out.print("창고 이름: ");
         userAndWarehouse.setWhName(sc.nextLine());
         System.out.print("창고 주소: ");
@@ -184,12 +192,13 @@ public class LoginView {
     /* 가맹점 관리 메소드 */
     private void branchControl() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("==================");
-        System.out.println(ANSI_BLUE +"1. 가맹점 조회"+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"2. 가맹점 추가"+ANSI_RESET);
-        System.out.println(ANSI_BLUE +"3. 가맹점 삭제"+ANSI_RESET);
-        System.out.println("0. 이전 메뉴");
-        System.out.print("관리할 메뉴 선택: ");
+        System.out.println("==== 가맹점관리 ===================================================================================");
+        System.out.println("1. 가맹점 조회");
+        System.out.println("2. 가맹점 추가");
+        System.out.println("3. 가맹점 삭제");
+        System.out.println("9. 이전으로");
+        System.out.println("=================================================================================================");
+        System.out.print("관리할 메뉴를 선택하세요: ");
         int num = sc.nextInt();
         sc.nextLine();
 
@@ -200,20 +209,20 @@ public class LoginView {
                 addUserController.createNewBranch(newUserAndWarehouse);
                 break;
             case 3: addUserController.deleteBranch(deleteBranch());
-            case 0:
+            case 9:
                 return;
             default:
-                System.out.println("잘못 입력하셨습니다.");
+                System.out.println("잘못된 값입니다. 다시 입력해주세요.");
         }
     }
 
     /* 가맹점 삭제 기능 */
     private Map<String, String> deleteBranch() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("=== 현재 등록된 가맹점 ====");
+        System.out.println("=== 현재 등록된 가맹점 =============================================================================");
         addUserController.showAllBranch();
-        System.out.println("======================");
-        System.out.print("삭제할 가맹점 번호: ");
+        System.out.println("=================================================================================================");
+        System.out.print("삭제할 가맹점 번호를 입력해주세요. : ");
         String no = sc.nextLine();
 
         Map<String, String> delete = new HashMap<>();
