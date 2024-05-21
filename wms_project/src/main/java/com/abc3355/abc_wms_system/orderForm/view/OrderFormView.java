4b93b613fea5fad994ff0ab93b0e9f20ab2d3047
DISNOTACAT <yeonjh915@gmail.com>
2024-05-21 12:53:27 +0900

    #6 view 루프 동작 수정 완료

--- wms_project/src/main/java/com/abc3355/abc_wms_system/orderForm/view/OrderFormView.java
+++ wms_project/src/main/java/com/abc3355/abc_wms_system/orderForm/view/OrderFormView.java
@@ -8,31 +8,47 @@ import java.util.*;
 
 public class OrderFormView {
     private OrderFormController orderFormController = new OrderFormController();
-
+    int result = 0;
 
     public void orderFormMain() {
 
-            Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+
+        while (result != 3) {
 
             System.out.print("새 주문서를 작성합니다. 진행하려면 '예'를 입력해주세요 : ");
             String input = sc.nextLine();
 
-            if (input.toUpperCase().equals("예")) {
-            do{
-                Map<String,String> para;
+            if (input.equals("예")) {
+                Map<String, String> para;
                 para = inputNewOrder();
-                checkOrder(para);            // 주문을 (확정진행/작성(수정)/취소) 할지를 확인 / 취소 == 0
-                } while (true);
+                result = checkOrder(para);
+                System.out.println("result = " + result);
+                switch (result) {            // 주문을 (확정진행/작성(수정)/취소) 할지를 확인 / 취소 == 0
+                    case 1:
+                        orderFormController.insertOrderByNo(para); // 입력한 주문을 테이블에 INSERT
+                        result = 0;
+                        break;
+                    case 2:
+                        result = 0;
+                        break;
+                    default:
+                        System.out.println("감사합니다. 주문신청이 종료되었습니다.");
+                        return;
+                }
             } else {
                 System.out.println("주문신청이 취소되었습니다.");
+                return;
             }
+        }
     }
 
     /**
      * 입력한 주문을 (확정진행/재작성/취소) 할지를 확인
+     *
      * @param para :  상품명(name), 색상(color), 사이즈(size), 수량(amount)
      */
-    public void checkOrder(Map<String, String> para) {
+    public int checkOrder(Map<String, String> para) {
 
         PrintResult pr = new PrintResult();
         Scanner sc = new Scanner(System.in);
@@ -43,7 +59,7 @@ public class OrderFormView {
         input.setSize(Integer.parseInt(para.get("size")));
         input.setProductAmount(Integer.parseInt(para.get("amount")));
 
-        do {
+        while (result == 0) {
 //            pr.printOrder(input);   // 주문 확인서 출력
             System.out.println("[주문 확정 하시겠습니까?]");
             System.out.println("1. 주문 확정");
@@ -52,27 +68,31 @@ public class OrderFormView {
             String no = sc.nextLine();
 
             switch (no) {
-                case "1" :
+                case "1":
                     System.out.println("주문을 저장합니다.");
-                    orderFormController.insertOrderByNo(para); // 입력한 주문을 테이블에 INSERT
+                    result = 1;
                     break;
-                case "2" :
+                case "2":
                     System.out.println("주문을 수정합니다.");
-                    orderFormMain(); // 주문메뉴 재전송
+                    result = 2;
                     break;
-                case "3" : break;
+                case "3":
+                    result = 3;
+                    break;
                 default:
                     System.out.println("잘못된 값입니다. 다시 입력해주세요.");
             }
-        }while (true);
+        }
+        return result;
     }
 
 
     /**
      * 상품명(name), 색상(color), 사이즈(size), 수량(amount)를 얻는 메서드
-     * @return Map<String,String> parameter
+     *
+     * @return Map<String, String> parameter
      */
-    public Map<String,String> inputNewOrder() {
+    public Map<String, String> inputNewOrder() {
         Scanner sc = new Scanner(System.in);
         Map<String, String> parameter = new HashMap<>();
 
@@ -84,8 +104,10 @@ public class OrderFormView {
         do {
             System.out.println("상품명을 입력하세요 : ");
             String name = sc.nextLine();
-            parameter.put("name",name);
+            parameter.put("name", name);
 
+            //  상품명을 검색했을 때, 상품명을 출력하고 선택하는 방식으로 진행할 필요 있음.
+            //  현재, '에어' 를 입력하면 에어포스 단일 상품만 조회되어서 주문이 진행됨
             if (orderFormController.printInvByProductName(name) == 0) {
                 System.out.println("검색한 상품이 존재하지 않습니다. 다시 검색해주세요.");
             } else {
@@ -93,18 +115,21 @@ public class OrderFormView {
             }
         } while (true);
 
+        // 1-1. 재고 리스트를 확인 후 상품 번호를 입력???
+
+
         // 2. 옵션 입력
         do {
             System.out.println("색상과 사이즈를 입력하세요(예: RED, 230) :");
             String[] strArr = (sc.nextLine()).split(",");
-            parameter.put("color",strArr[0].toUpperCase().trim());
-            parameter.put("size",strArr[1].trim());
+            parameter.put("color", strArr[0].toUpperCase().trim());
+            parameter.put("size", strArr[1].trim());
 
             if (orderFormController.checkOpt(parameter) == 0) {
                 System.out.println("선택한 옵션이 존재하지 않습니다. 다시 검색해주세요.");
             } else {
-                parameter.put("color",strArr[0].toUpperCase());
-                parameter.put("size",strArr[1]);
+                parameter.put("color", strArr[0].toUpperCase().trim());
+                parameter.put("size", strArr[1].trim());
                 break;
             }
         } while (true);
@@ -114,7 +139,7 @@ public class OrderFormView {
         do {
             System.out.println("수량을 입력하세요 :");
             String amount = sc.nextLine();
-            parameter.put("amount",amount);
+            parameter.put("amount", amount);
 
             int result = orderFormController.checkInvAmount(parameter);
 
